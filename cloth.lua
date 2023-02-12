42d98393168e3d3894f7bc323aeb2b40a24d953f
fouR <irgiamysebi@gmail.com>
2023-02-13 04:30:33 +0700

    template4

---
+++ cloth.lua
@@ -0,0 +1,11 @@
+function ResetMalePeds() -- idk how to put back clothes standalone version
+  TriggerServerEvent("qb-clothes:loadPlayerSkin") -- LOADING PLAYER'S CLOTHES
+  TriggerServerEvent("qb-clothing:loadPlayerSkin") -- LOADING PLAYER'S CLOTHES
+  -- TriggerEvent("fivem-appearance:client:reloadSkin") -- IF USING FIVEM-APPEARANCE UNCOMENT THIS
+end
+
+function ResetFemalePeds() -- idk how to put back clothes standalone version
+  TriggerServerEvent("qb-clothes:loadPlayerSkin") -- LOADING PLAYER'S CLOTHES
+  TriggerServerEvent("qb-clothing:loadPlayerSkin") -- LOADING PLAYER'S CLOTHES
+  -- TriggerEvent("fivem-appearance:client:reloadSkin") -- IF USING FIVEM-APPEARANCE UNCOMENT THIS
+end
